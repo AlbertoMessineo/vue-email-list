fe8e05e63adf92a3b0203a07fbfd435aef5b3804
AlbertoMessineo <albevorox@gmail.com>
2023-12-05 18:38:00 +0100

    feat: es completed

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-email-list
+
+Nell'esercizio di oggi andremo a rappresentare in pagina un elenco di mail generati dall'api 'https://flynn.boolean.careers/exercises/api/random/mail' mediante vue js;
--- main.js
+++ main.js
@@ -5,16 +5,26 @@ const { createApp } = Vue;
 createApp({
     data() {
       return {
-        apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail'
+        apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
+        mails: [],
+
       };
+    },
+    mounted(){
+        this.mailGenerator()
     },
     methods:{
         mailGenerator(){
             for(let i = 0; i < 10; i++){
-                axios.get(this.apiUrl);
+                axios.get(this.apiUrl)
+                    .then(response => { 
+                        console.log(response.data.response);
+                        this.mails.push(response.data.response);
+                    });
             }
         },
-        
-
+        reloadPage() {
+            window.location.reload();
+          }
     },
   }).mount('#app')
