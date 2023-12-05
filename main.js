a5eccf9c51a30e8a6c67458d1f626df150b9ff78
AlbertoMessineo <albevorox@gmail.com>
2023-12-05 17:39:19 +0100

    feat: js app created

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+      return {
+        apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail'
+      };
+    },
+    methods:{
+        mailGenerator(){
+            for(let i = 0; i < 10; i++){
+                axios.get(this.apiUrl);
+            }
+        },
+        
+
+    },
+  }).mount('#app')
